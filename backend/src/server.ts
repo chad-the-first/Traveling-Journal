c0e3f48fcffe0dae3da85e6688d6e89b0167c712
Chad <worktannoury@gmail.com>
2023-08-03 15:00:55 +0300

    start

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+    res.send("ya habibi!");
+});
+
+app.listen(port, () => {
+    console.log("server running on port " + port);
+})
