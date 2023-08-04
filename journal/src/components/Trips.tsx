ff1cc100f8137181688fcf74a6123d96168db446
Chad <worktannoury@gmail.com>
2023-08-04 18:08:21 +0300

    Styling

--- journal/src/components/Trips.tsx
+++ journal/src/components/Trips.tsx
@@ -1,29 +1,35 @@
 import styles from "../styles/Trip.module.css";
-import { Card, Row, Col } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { Trip as TripModel } from "../models/trip";
+import { formatDate } from "../utils/formatDate";
 
 interface props {
   trip: TripModel;
+  className?: string;
 }
 
-const Trips = ({ trip }: props) => {
+const Trips = ({ trip, className }: props) => {
   const { title, body, author, meta, createdAt, updatedAt } = trip;
 
+  let createdUpdatedText: string;
+  if (updatedAt > createdAt) {
+    createdUpdatedText = "Updated: " + formatDate(updatedAt);
+  } else {
+    createdUpdatedText = "Created: " + formatDate(createdAt);
+  }
+
   return (
-    <Row xs={1} md={2} lg={3} className="g-4">
-      <Col>
-        <Card className={styles.tripCard}>
-          <Card.Img variant="top" src="https://placehold.co/400x200" />
-          <Card.Body>
-            <Card.Title>{title}</Card.Title>
-            <Card.Text className={styles.cardText}>{body}</Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">by: {author}</small>
-          </Card.Footer>
-        </Card>
-      </Col>
-    </Row>
+    <Card className={`${styles.tripCard} ${className}`}>
+      <Card.Img variant="top" src="https://placehold.co/400x200" />
+      <Card.Body className={styles.cardBody}>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text className={styles.cardText}>{body}</Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <small className="text-muted float-start">by: {author} </small>
+        <small className="text-muted float-end">{createdUpdatedText}</small>
+      </Card.Footer>
+    </Card>
   );
 };
 
