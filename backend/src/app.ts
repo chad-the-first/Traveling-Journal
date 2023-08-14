992979c603fc7eda8411f39106833019716a44e6
Chad <worktannoury@gmail.com>
2023-08-14 17:49:39 +0300

    change cookie setting to none

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -23,7 +23,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         maxAge: 60 * 60 * 1000,
-        sameSite: "lax"
+        sameSite: "none"
     },
     rolling: true,
     store: MongoStore.create({
